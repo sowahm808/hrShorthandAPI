c383810a0e4bb94b09943843cdbb8f498bc4cda2
Michael Sowah <sowahm808@gmail.com>
2025-07-06 13:38:37 -0500

    updated cors

--- config/cors.php
+++ config/cors.php
@@ -17,8 +17,8 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => array_filter(explode(',', env('CORS_ALLOWED_ORIGINS', 'http://localhost:4200'))),
-
+    'allowed_origins' => ['*'],
+    
     'allowed_origins_patterns' => [],
 
     'allowed_headers' => ['*'],
